6bb12156010e25940246f1c7eb49091d11c4d4d1
sunxim <sunxiiim@gmail.com>
2024-05-31 18:50:44 +0600

    re implementing server instance

--- server/index.js
+++ server/index.js
@@ -37,6 +37,94 @@
 //   console.log(`GraphQL Server running at http://localhost:${port}/graphql 🚀`);
 // });
 
+// v2
+// import { ApolloServer } from "@apollo/server";
+// import { expressMiddleware } from "@apollo/server/express4";
+// import cors from "cors";
+// import * as dotenv from "dotenv";
+// import express from "express";
+// import http from "http";
+// import { Server } from "socket.io";
+// import { resolvers, typeDefs } from "./graphql/graphql.js";
+// dotenv.config();
+
+// const port = process.env.PORT || 4000;
+// const cors_origin = [process.env.LOCAL, process.env.PRODUCTION];
+// const app = express();
+// app.use(
+//   cors({
+//     origin: cors_origin,
+//     credentials: true,
+//   })
+// );
+// // app.use((req, res, next) => {
+// //   res.header({ "Access-Control-Allow-Origin": "*" });
+// //   next();
+// // });
+
+// const httpServer = http.createServer(app);
+
+// const server = new ApolloServer({
+//   typeDefs: typeDefs,
+//   resolvers: resolvers,
+// });
+
+// await server.start();
+
+// export const io = new Server(httpServer, {
+//   cors: {
+//     origin: cors_origin,
+//   },
+// });
+
+// app.use(express.json());
+
+// const context = ({ req }) => {
+//   return req;
+// };
+
+// app.use("/graphql", expressMiddleware(server, { context }));
+
+// export const onlineUsers = {};
+
+// io.on("connection", (socket) => {
+//   console.log("A user connected");
+
+//   socket.on("join", (userId) => {
+//     onlineUsers[userId] = socket.id;
+//     socket.userId = userId;
+//     io.emit("online-users", onlineUsers);
+//   });
+
+//   // socket.on("send-message", ({ message, to, from }, ack) => {
+//   //   const toSocketId = onlineUsers[to];
+//   //   if (toSocketId) {
+//   //     socket
+//   //       .to(toSocketId)
+//   //       .emit("receive-message", { text: message, sender: from });
+//   //     ack({ success: true });
+//   //   }
+//   // });
+
+//   socket.on("disconnect", () => {
+//     delete onlineUsers[socket.userId];
+//     io.emit("online-users", onlineUsers);
+//     console.log("User disconnected");
+//   });
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.json({ hello: "hello" });
+// });
+
+// httpServer.listen(port, () => {
+//   console.log(`Express Server running at http://localhost:${port} 🚀`);
+//   console.log(`GraphQL Server running at http://localhost:${port}/graphql 🚀`);
+// });
+
+// export default server;
+
+// v3
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import cors from "cors";
@@ -45,21 +133,20 @@ import express from "express";
 import http from "http";
 import { Server } from "socket.io";
 import { resolvers, typeDefs } from "./graphql/graphql.js";
+
 dotenv.config();
 
 const port = process.env.PORT || 4000;
 const cors_origin = [process.env.LOCAL, process.env.PRODUCTION];
-const app = express();
+let app = express();
+app.use(express.json());
+
 app.use(
   cors({
     origin: cors_origin,
     credentials: true,
   })
 );
-// app.use((req, res, next) => {
-//   res.header({ "Access-Control-Allow-Origin": "*" });
-//   next();
-// });
 
 const httpServer = http.createServer(app);
 
@@ -68,57 +155,54 @@ const server = new ApolloServer({
   resolvers: resolvers,
 });
 
-await server.start();
+// Function to start ApolloServer
+const startApolloServer = async () => {
+  await server.start();
 
-export const io = new Server(httpServer, {
-  cors: {
-    origin: cors_origin,
-  },
-});
+  const context = ({ req }) => {
+    return req;
+  };
 
-app.use(express.json());
+  app.use("/graphql", expressMiddleware(server, { context }));
 
-const context = ({ req }) => {
-  return req;
+  const io = new Server(httpServer, {
+    cors: {
+      origin: cors_origin,
+    },
+  });
+
+  const onlineUsers = {};
+
+  io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    socket.on("join", (userId) => {
+      onlineUsers[userId] = socket.id;
+      socket.userId = userId;
+      io.emit("online-users", onlineUsers);
+    });
+
+    socket.on("disconnect", () => {
+      delete onlineUsers[socket.userId];
+      io.emit("online-users", onlineUsers);
+      console.log("User disconnected");
+    });
+  });
+
+  app.get("/hello", (req, res) => {
+    res.json({ hello: "hello" });
+  });
+
+  httpServer.listen(port, () => {
+    console.log(`Express Server running at http://localhost:${port} 🚀`);
+    console.log(
+      `GraphQL Server running at http://localhost:${port}/graphql 🚀`
+    );
+  });
+
+  return { app, io, onlineUsers };
 };
 
-app.use("/graphql", expressMiddleware(server, { context }));
+const { app: apps, io, onlineUsers } = await startApolloServer();
 
-export const onlineUsers = {};
-
-io.on("connection", (socket) => {
-  console.log("A user connected");
-
-  socket.on("join", (userId) => {
-    onlineUsers[userId] = socket.id;
-    socket.userId = userId;
-    io.emit("online-users", onlineUsers);
-  });
-
-  // socket.on("send-message", ({ message, to, from }, ack) => {
-  //   const toSocketId = onlineUsers[to];
-  //   if (toSocketId) {
-  //     socket
-  //       .to(toSocketId)
-  //       .emit("receive-message", { text: message, sender: from });
-  //     ack({ success: true });
-  //   }
-  // });
-
-  socket.on("disconnect", () => {
-    delete onlineUsers[socket.userId];
-    io.emit("online-users", onlineUsers);
-    console.log("User disconnected");
-  });
-});
-
-app.get("/hello", (req, res) => {
-  res.json({ hello: "hello" });
-});
-
-httpServer.listen(port, () => {
-  console.log(`Express Server running at http://localhost:${port} 🚀`);
-  console.log(`GraphQL Server running at http://localhost:${port}/graphql 🚀`);
-});
-
-export default server;
+export { apps, io, onlineUsers };
